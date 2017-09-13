4bcc6eeb65d9e8010ff2f483cd3632af19caeaaf
dalazaro <daryl.lazaro@gmail.com>
2017-09-13 10:59:56 -0700

    completed Sprint 1: added bcrypt, auth, login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  include SessionsHelper
+
   # root to: 'application#home'
   def home
     render('../views/home')
