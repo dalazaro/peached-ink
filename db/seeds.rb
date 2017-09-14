6980a70b43782882d7b9f37d973dd191b3ff0775
dalazaro <daryl.lazaro@gmail.com>
2017-09-14 14:04:48 -0700

    added home page view of 3 most recent collections

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "Kean", last_name: "Saetern", username: "kean", email_address: "keansaetern@gmail.com", password: "password", password_confirmation: "password", bio: "Hello there! My name is Kean Saetern, and thank you for viewing my website. By day, I work for a molecular diagnostics company to serve and better help cancer patients. From these experiences, I’ve learned that no matter what darkness has emerged, hope is a powerful ally. Hope creates positive thoughts, brings happiness, and is contagious. Hope comes from many different sources. For me, it’s the small things: colors, cute details, and simple joys that bring big smiles. It is my sincere hope that my artwork brightens dark days and inspires a bit color in everyone!", artist_statement: "Life captured in cute and simple art form.")
