6a846c366eb3988ab015bf8edbf9e71ee935ef76
dalazaro <daryl.lazaro@gmail.com>
2017-09-14 13:31:56 -0700

    functioning multiple image upload

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,5 +1,18 @@
+require 'carrierwave'
+
 class Collection < ApplicationRecord
   belongs_to :user
+
+  mount_uploader :illustration01, IllustrationUploader
+  mount_uploader :illustration02, IllustrationUploader
+  mount_uploader :illustration03, IllustrationUploader
+  mount_uploader :illustration04, IllustrationUploader
+  mount_uploader :illustration05, IllustrationUploader
+  mount_uploader :illustration06, IllustrationUploader
+  mount_uploader :illustration07, IllustrationUploader
+  mount_uploader :illustration08, IllustrationUploader
+  mount_uploader :illustration09, IllustrationUploader
+  mount_uploader :illustration10, IllustrationUploader
 
   extend FriendlyId
   friendly_id :name
