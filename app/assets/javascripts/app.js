c191abfb9485df7d40100bec5f8d416c1c0c3c56
Daryl Lazaro <daryl.lazaro@gmail.com>
2017-09-15 23:54:05 -0700

    Update app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,13 +2,4 @@ $(document).ready(function() {
 
   $('.carousel').carousel();
 
-  $custom-file-text: (
-    placeholder: (
-      en: "Choose file...",
-    ),
-    button-label: (
-      en: "Browse",
-    )
-  );
-
 });
