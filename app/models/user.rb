b8139efc782cc262f8e8b666bc6227b3fa0b5404
dalazaro <daryl.lazaro@gmail.com>
2017-10-03 10:37:03 -0700

    refactored avatar to use imgur upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
-require 'carrierwave'
-
 class User < ApplicationRecord
   has_secure_password
   has_many :collections
-
-  mount_uploader :avatar, AvatarUploader
 
   def self.confirm(params)
     @user = User.find_by({email_address: params[:email_address]})
